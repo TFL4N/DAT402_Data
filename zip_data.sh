0541b98cef88e12bb525681493b4425f220a4c60
TFL4N <tflan10@gmail.com>
2025-04-23 14:15:08 -0400

    add unzip.sh to data.zip

--- zip_data.sh
+++ zip_data.sh
@@ -3,4 +3,4 @@
 OUTPUT=prod_data.zip
 
 rm $OUTPUT
-zip -r $OUTPUT LICENSE vert_right vert_left horz_right horz_left -x '**/.*' -x '**/__MACOSX' 
+zip -r $OUTPUT LICENSE unzip_data.sh vert_right vert_left horz_right horz_left -x '**/.*' -x '**/__MACOSX' 
